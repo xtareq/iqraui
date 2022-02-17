d6415d573cfc4de37fba188eccdd5dcf9e857085
Tareq Hossain <xtrinsic96@gmail.com>
2022-02-18 02:55:39 +0600

    motion added

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styled from 'styled-components'
-
+import {motion} from 'framer-motion'
 
 
 interface ButtonProps {
 	label: string;
 }
 
-const StyledButton = styled.button`
+const StyledButton = styled(motion.button)`
 	background-color:${props=>props.theme.colors? props.theme.colors.primary : 'indigo'};
 	color: ${props=>props.theme?props.theme.color:'white'};
 	font-sizeZ:14px;
@@ -15,7 +15,7 @@ const StyledButton = styled.button`
 `
 
 const Button = (props: ButtonProps) => {
-	return <StyledButton>{props.label}</StyledButton>
+	return <StyledButton transition={{duration:1}} initial={{x:-100}}  animate={{x:0}} >{props.label}</StyledButton>
 }
 
 export default Button;
