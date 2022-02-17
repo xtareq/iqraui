b6b1c3c6288c5c9677a2150e3621211175297c5c
Tareq Hossain <xtrinsic96@gmail.com>
2022-02-18 02:43:37 +0600

    added styled components

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import './Button.css';
+import styled from 'styled-components'
+
+
 
 interface ButtonProps {
 	label: string;
 }
 
+const StyledButton = styled.button`
+	background-color:${props=>props.theme.colors? props.theme.colors.primary : 'indigo'};
+	color: ${props=>props.theme?props.theme.color:'white'};
+	font-sizeZ:14px;
+	border:1px solid red;
+`
+
 const Button = (props: ButtonProps) => {
-	return <button>{props.label}</button>
+	return <StyledButton>{props.label}</StyledButton>
 }
 
 export default Button;
