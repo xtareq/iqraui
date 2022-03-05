3a30d150349e151be5fa18f5153ddf344547b1b1
Tareq Hossain <xtrinsic96@gmail.com>
2022-03-06 04:00:09 +0600

    update components

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,17 @@
-
-
+export * from './Badge';
 export * from './Button';
+export * from './Card';
+export * from './Checkbox';
+export * from './Divider';
+export * from './Drawer';
+export * from './Dropdown';
+export * from './Flex';
+export * from './Grid';
+export * from './Input';
+export * from './Layout';
+export * from './Menu';
+export * from './Modal';
 export * from './Row';
+export * from './Select';
+export * from './Space';
 export * as Theme from './Theme'
